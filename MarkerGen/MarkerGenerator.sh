d1aafe4c3494302b10ac9249d377615f2e025c32
illixion <illixion@illixion.com>
2018-04-12 18:29:50 +0300

    very minor code tidy-up

--- MarkerGen/MarkerGenerator.sh
+++ MarkerGen/MarkerGenerator.sh
@@ -13,10 +13,9 @@
 # TIMEBASE=1/1 - precision control. best to be left at seconds although you can be even more precise by adding 0's
 # START=10
 # END=25 - do not omit
-# title= - title
+# title= - track title to be shown in the player
 
 function buildmetadata {
-	cmd.exe /c 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' "https://fridaynighttech.com/episodes/$name/"
 	read -e -p "Starting point: " -i "100" spoint # Intro length. newer: 100 older: 65. change occured at episode 125
 	echo -e '\n[CHAPTER]\nTIMEBASE=1/1\nSTART='0'\nEND='$spoint'\ntitle='"Intro" >> "$dstpath/${name}_metadata"
 	read -p "Starting line: " line # first track line number in tweets.csv
@@ -26,9 +25,9 @@ function buildmetadata {
 		title="$(sed "${line}q;d" $tweets)"
 		echo "$title"
 		echo -e "[CHAPTER]\nTIMEBASE=1/1" >> $dstpath/${name}_metadata
-		cdate=$(sed "$((line))q;d" /mnt/c/Users/Ray/Desktop/tweets/tweets_formatted.csv | cut -c1-19)
-		ndate=$(sed "$((line-1))q;d" /mnt/c/Users/Ray/Desktop/tweets/tweets_formatted.csv | cut -c1-19)
-		diff=$(python3 /mnt/d/Users/Ray/MEGA/Scripts/FNT_tag_project/TimeDiff.py $cdate $ndate)
+		cdate=$(sed "$((line))q;d" $tweets | cut -c1-19)
+		ndate=$(sed "$((line-1))q;d" $tweets | cut -c1-19)
+		diff=$(python3 TimeDiff.py $cdate $ndate)
 		end=$(($prev+diff))
 		echo 'START='$prev >> $dstpath/${name}_metadata
 		echo 'END='$end >> $dstpath/${name}_metadata
@@ -41,12 +40,11 @@ function buildmetadata {
 	unset done
 }
 
-tweets="/mnt/c/Users/Ray/Desktop/tweets/tweets_formatted.csv"
+tweets="/mnt/c/Users/Ray/Documents/GitHub/XBN/XanaNP-tweets/tweets_formatted.csv"
 srcpath=/mnt/d/Users/Ray/MoreMusic/FNT
 dstpath=/mnt/d/Users/Ray/MoreMusic/FNT/metadata
 delete=0
 
-# change container from mp3 to m4a
 for file in $srcpath/*.mp3; do
 	name=`basename $file | cut -d'.' -f1`;
 	echo $name;
@@ -55,7 +53,7 @@ for file in $srcpath/*.mp3; do
 	# process csv here
 	if [[ $? != 0 ]]; then read -e -p "Skip this file? [y/n] " -i "y" skipit ; fi
 	if [[ $skipit != y ]]; then echo "$name" ; buildmetadata; else skipit=n ;  fi
-	read -e -p "Processing done. Modify? " -i "n" modify # we want to have an option of modification in case tweets.csv doesn't line up correctly
+	read -e -p "Processing done. Modify? " -i "n" modify # sometimes tweets don't line up correctly
 	if [[ "$modify" != "n" ]]; then
 		modify=n
 		cmd.exe /c "C:\Program Files (x86)\Notepad++\notepad++.exe" 'D:\Users\Ray\MoreMusic\FNT\metadata\'"$name"'_metadata'
