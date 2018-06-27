f14193fe5b7b8720fce023a3521153035e34f879
s0ph0s-2 <alexander.g.langley@gmail.com>
2018-06-26 23:00:55 -0400

    Finish CUE import, add converter script

---
+++ convertmarks.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Convert markers between types.
+"""
+
+from PostShowV2 import MCS
+import argparse
+import sys
+
+
+def determine_type(filename: str) -> int:
+    if filename.endswith(".txt"):
+        return MCS.AUDACITY
+    elif filename.endswith(".cue"):
+        return MCS.CUE
+    elif filename.endswith(".lrc"):
+        return MCS.LRC
+    else:
+        raise ValueError("Unsupported file type.")
+
+
+def main(argv: list):
+    parser = argparse.ArgumentParser(description="Convert marker types.")
+    parser.add_argument('in_file', help="the input marker file. Audacity "
+                                        "Labels and LRC currently supported.")
+    parser.add_argument('out_file', help="the output marker file. CUE and LRC "
+                                         "currently supported.")
+    parser.add_argument('-m', '--media-file', default=None,
+                        help="the media file with which these markers are "
+                             "associated. Required for CUE output, ignored "
+                             "for others.")
+    namespace = parser.parse_args()
+    out_type = determine_type(namespace.out_file)
+    if out_type == MCS.CUE and namespace.media_file is None:
+        raise ValueError("You must pass --media-file when converting to CUE.")
+    mcs = MCS(media_filename=namespace.media_file)
+    mcs.load(namespace.in_file)
+    mcs.save(namespace.out_file, out_type)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
