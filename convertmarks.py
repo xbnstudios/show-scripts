a41f02ce2b5553717b8a5b7331d531dc6e589f72
s0ph0s-2 <s0ph0s-2@users.noreply.github.com>
2019-05-08 14:32:48 -0400

    Add FFMETADATA1 output format

--- convertmarks.py
+++ convertmarks.py
@@ -15,24 +15,37 @@ def determine_type(filename: str) -> int:
         return MCS.CUE
     elif filename.endswith(".lrc"):
         return MCS.LRC
+    elif filename.endswith(".ffmetadata1"):
+        return MCS.FFMETADATA1
     else:
         raise ValueError("Unsupported file type.")
 
 
 def main(argv: list):
     parser = argparse.ArgumentParser(description="Convert marker types.")
-    parser.add_argument('in_file', help="the input marker file. Audacity "
-                                        "Labels and LRC currently supported.")
-    parser.add_argument('out_file', help="the output marker file. CUE and LRC "
-                                         "currently supported.")
-    parser.add_argument('-m', '--media-file', default=None,
-                        help="the media file with which these markers are "
-                             "associated. Required for CUE output, ignored "
-                             "for others.")
+    parser.add_argument(
+        "in_file",
+        help="the input marker file. Audacity " "Labels and LRC currently supported.",
+    )
+    parser.add_argument(
+        "out_file", help="the output marker file. CUE and LRC " "currently supported."
+    )
+    parser.add_argument(
+        "-m",
+        "--media-file",
+        default=None,
+        help="the media file with which these markers are "
+        "associated. Required for CUE output, ignored "
+        "for others.",
+    )
     namespace = parser.parse_args()
     out_type = determine_type(namespace.out_file)
-    if out_type == MCS.CUE and namespace.media_file is None:
-        raise ValueError("You must pass --media-file when converting to CUE.")
+    if (
+        out_type == MCS.CUE or out_type == MCS.FFMETADATA1
+    ) and namespace.media_file is None:
+        raise ValueError(
+            "You must pass --media-file when converting to CUE or FFMETADATA1."
+        )
     mcs = MCS(media_filename=namespace.media_file)
     mcs.load(namespace.in_file)
     mcs.save(namespace.out_file, out_type)
